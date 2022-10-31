436d8c52bc264d80caf713e2ae8ccbf6f905f76b
Threalmohh <laaromohammed1@gmail.com>
2022-10-31 19:48:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+ 
+function getRandomNumbers() {
+  return Math.floor(Math.random() * colors.length);
+}
+
+btn.addEventListener ('click', function () {
+  const randomNumber = getRandomNumbers();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
